14e20f0f87bbc97b61b6fb58b690f5a7d9af626c
Raptorex65 <71727239+Raptorex65@users.noreply.github.com>
2021-09-17 21:52:27 +0200

    section-5 modified

--- models/item.model.js
+++ models/item.model.js
@@ -4,43 +4,55 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     Name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+    },
+    Category: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     Description: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
-   
+
     Address: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-    City: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-    Canton: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-    
-    BundleItemPoint: {
-      type:DataTypes.INTEGER,
-      allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
+    City: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Canton: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Postal_code: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
     createdAt: {
       allowNull: false,
-        type: DataTypes.DATE
-      },
+      type: DataTypes.DATE,
+    },
     updatedAt: {
       allowNull: false,
-        type: DataTypes.DATE
-      }
-    });
+      type: DataTypes.DATE,
+    },
+  });
 
   return Item;
 };
